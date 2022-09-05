8b2835101449eaa5ceba466335ea33ba1c252649
valentin <valentinvacher01@gmail.com>
2022-09-05 19:59:39 +0200

    navbar update

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -8,7 +8,7 @@ const header = document.getElementById("header")
 let nav = document.createElement("nav");
 let logo = document.createElement("a");
 logo.href = "#";
-logo.className = "logo"
+logo.className = "logo";
 
 let logoImg = document.createElement("img");
 logoImg.src = "./asset/images/logo.png";
@@ -19,22 +19,38 @@ nav.appendChild(logo);
 
 let cv = document.createElement('a');
 cv.href = "./asset/resource/CV_développeur_Informatique.pdf";
-cv.classList = "cv";
-cv.innerText = "CV EN PDF"
+cv.target = "_blank"
+cv.className = "cv";
+cv.innerText = "CV EN PDF";
 
 nav.appendChild(cv);
 
 let contact = document.createElement("a");
 contact.href = "#";
 contact.className = "contact";
-contact.innerText = "CONTACT"
+contact.innerText = "CONTACT";
 
 nav.appendChild(contact);
 
 let bomberman = document.createElement("a");
 bomberman.href = "#";
 bomberman.className = "bomberman";
-bomberman.innerText = "BOMBERMAN"
+bomberman.innerText = "BOMBERMAN";
 
-nav.appendChild(bomberman)
+nav.appendChild(bomberman);
+
+let gridGitHub = document.createElement("dive");
+gridGitHub.className = "grid-github";
+
+let gitHubLink = document.createElement("a");
+gitHubLink.href = "https://github.com/ValentinVacher";
+
+let gitHub = document.createElement("i");
+gitHub.className = "fa-brands fa-github";
+gitHubLink.target = "_blank"
+
+gitHubLink.appendChild(gitHub);
+gridGitHub.appendChild(gitHubLink);
+nav.appendChild(gridGitHub)
+
 header.appendChild(nav);
