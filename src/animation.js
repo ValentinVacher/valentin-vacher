de1abfbb57a0b58d144bc81f2dccb7f47216a6ad
valentin <valentinvacher01@gmail.com>
2023-01-22 14:06:31 +0100

    arrow update

--- src/animation.js
+++ src/animation.js
@@ -1,6 +1,7 @@
 const bac = document.getElementById("bac");
 const licence = document.getElementById("licence")
 const autoformation = document.getElementById("autoformation");
+const NET = document.getElementById("NET");
 
 parallax.addEventListener('scroll', () => {
     const {scrollTop, clientHeight} = document.documentElement;
@@ -8,6 +9,7 @@ parallax.addEventListener('scroll', () => {
     const toBacToTopViewport = bac.getBoundingClientRect().top;
     const toLicenceToTopViewport = licence.getBoundingClientRect().top;
     const topAutoformationToViewport = autoformation.getBoundingClientRect().top;
+    const topNETToViewport = NET.getBoundingClientRect().top;
 
     if(scrollTop > (scrollTop + toBacToTopViewport).toFixed() - clientHeight * 0.8 && scrollTop < (scrollTop + toBacToTopViewport).toFixed() - clientHeight * 0.3){
         bac.className = 'right-active visible';
@@ -28,5 +30,12 @@ parallax.addEventListener('scroll', () => {
     }
     else if(scrollTop > (scrollTop + topAutoformationToViewport).toFixed() - clientHeight * 0.2 || (scrollTop < (scrollTop + topAutoformationToViewport).toFixed() - clientHeight * 0.9 && autoformation.className === 'right-active visible')){
         autoformation.className = 'reverse-right-active';
+    }
+
+    if(scrollTop > (scrollTop + topNETToViewport).toFixed() - clientHeight * 0.8 && scrollTop < (scrollTop + topNETToViewport).toFixed() - clientHeight * 0.3){
+        NET.className = 'left-active visible';
+    }
+    else if(scrollTop > (scrollTop + topNETToViewport).toFixed() - clientHeight * 0.2 || (scrollTop < (scrollTop + topNETToViewport).toFixed() - clientHeight * 0.9 && NET.className === 'left-active visible')){
+        NET.className = 'reverse-left-active';
     }
 })
