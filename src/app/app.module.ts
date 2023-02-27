8e3ef85e9fa12a813f2d9f37e16d238c560b0549
valentin <valentinvacher01@gmail.com>
2023-02-27 16:00:23 +0100

    parallax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ParallaxeComponent } from './parallaxe/parallaxe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParallaxeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/parallaxe/parallaxe.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parallaxe',
+  templateUrl: './parallaxe.component.html',
+  styleUrls: ['./parallaxe.component.scss']
+})
+export class ParallaxeComponent {
+}
