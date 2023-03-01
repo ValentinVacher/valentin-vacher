a41f6e126304f6a8f03c0418e1808662cc70b2df
valentin <valentinvacher01@gmail.com>
2023-03-01 14:57:41 +0100

    nav update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
@@ -6,6 +6,23 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   githubIcon = faGithub
+  hover!: string;
+
+  ngOnInit() {
+      this.hover = "1";
+  }
+
+  onMouseEnter(){
+    this.hover = "2";
+  }
+
+  onMouseLeave(){
+    this.hover ="1";
+  }
+
+  onClick(){
+    this.hover ="1";
+  }
 }
